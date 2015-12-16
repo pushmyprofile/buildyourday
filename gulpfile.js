470cb282648bd4ff5e79cbdfd72d678c0810a23a
pushmyprofile <nick@pushmyprofile.co.uk>
2015-12-16 15:39:20 +0000

    Add JS Files For Project
    Added Vendor Js files including jquery alomg with motion ui for
    animations

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var elixir = require('laravel-elixir');
 
+var path = {
+    foundation: 'foundation-sites/',
+    motionui:   'motion-ui/',
+    bower:  'bower_components/',
+    motionui: 'motion-ui/'
+};
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -18,7 +25,7 @@ elixir(function(mix) {
     'app.scss', 
     'public/css/app.css',
     { includePaths:
-        ['bower_components/foundation-sites/scss/']  
+        ['bower_components/foundation-sites/scss/', path.bower + path.motionui + '/src/',]  
     });
 
 
@@ -27,5 +34,32 @@ elixir(function(mix) {
         'main.scss'
     ], './public/css/main.css');
 
+
+     
+    mix.scripts([
+
+        // vendor
+        path.bower + 'jquery/dist/jquery.js',
+        path.bower + 'what-input/what-input.js',
+
+        // foundation core
+        // path.bower + path.foundation + 'dist/foundation.js',
+        path.bower + path.foundation + 'js/foundation.core.js',
+        path.bower + path.foundation + 'js/foundation.util.mediaQuery.js',
+        // foundation plug-ins
+        path.bower + path.foundation + 'js/foundation.util.box.js',
+        path.bower + path.foundation + 'js/foundation.util.nest.js',
+        path.bower + path.foundation + 'js/foundation.util.motion.js',
+        path.bower + path.foundation + 'js/foundation.util.keyboard.js',
+        path.bower + path.foundation + 'js/foundation.util.triggers.js',
+        path.bower + path.foundation + 'js/foundation.abide.js',
+        path.bower + path.foundation + 'js/foundation.dropdownMenu.js',
+        path.bower + path.foundation + 'js/foundation.responsiveMenu.js',
+        path.bower + path.foundation + 'js/foundation.responsiveToggle.js',
+        path.bower + path.foundation + 'js/foundation.toggler.js',
+        path.bower + path.motionui + 'dist/motion-ui.js',
+   
+
+    ], 'public/js/vendor.js', './');
 
 }); 
--- resources/views/partials/home-header.blade.php
+++ resources/views/partials/home-header.blade.php
@@ -1,4 +1,13 @@
 <div class="hero">
 @include('partials.nav')
+<h1 class="linear spin-in mui-enter mui-enter-active">Build. Invite. Connect.</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis congue magna. Donec in metus rhoncus velit hendrerit mattis quis a nunc. Suspendisse sem arcu, laoreet in arcu sed, eleifend ultrices felis. Nulla in urna nec odio porta placerat. </p>
+<div class="valign-wrapper">
+	<div class="valign">
+<a href="{{ route('register') }}" class="button large">Create Your Event</a>
+<a href="{{ route('register') }}" class="button large">RSVP</a>
+</div>
+</div>
 </div>
  
